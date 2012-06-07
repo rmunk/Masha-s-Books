62c55a50574771768c7646307b0d0d7be28ed88a
Ranko Munk <ranko.munk@gmail.com>
2012-06-07 18:54:15 +0200

    Commit nakon restora sustava

---
+++ Masha's Books/Masha's Books/BookDownloader.h
@@ -0,0 +1,13 @@
+//
+//  BookDownloader.h
+//  Masha's Books
+//
+//  Created by Ranko Munk on 6/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BookDownloader : NSObject
+
+@end
---
+++ Masha's Books/Masha's Books/BookDownloader.m
@@ -0,0 +1,13 @@
+//
+//  BookDownloader.m
+//  Masha's Books
+//
+//  Created by Ranko Munk on 6/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BookDownloader.h"
+
+@implementation BookDownloader
+
+@end
