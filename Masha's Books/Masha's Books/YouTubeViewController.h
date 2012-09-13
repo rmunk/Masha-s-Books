ed749a0bd8f132c6e8c9a2d764f3a2964bee8a8a
Luka Miljak <luka.miljak@gmail.com>
2012-09-14 00:11:28 +0200

    Commit prije pusha.

---
+++ Masha's Books/Masha's Books/YouTubeViewController.h
@@ -0,0 +1,13 @@
+//
+//  YouTubeViewController.h
+//  Masha's Books
+//
+//  Created by Luka Miljak on 9/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YouTubeViewController : UIViewController
+
+@end
---
+++ Masha's Books/Masha's Books/YouTubeViewController.m
@@ -0,0 +1,49 @@
+//
+//  YouTubeViewController.m
+//  Masha's Books
+//
+//  Created by Luka Miljak on 9/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "YouTubeViewController.h"
+
+@interface YouTubeViewController ()
+
+@end
+
+@implementation YouTubeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft || interfaceOrientation == UIInterfaceOrientationLandscapeRight) {
+        return YES;
+    } else if (interfaceOrientation == UIInterfaceOrientationPortrait || interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+@end
